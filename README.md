045fc09561f829725fc0de4f8910133a68760909
Quentin Spencer-Harper <qspencer-harper@palantir.com>
2018-06-07 21:58:53 +0100

    First working version

---
+++ README.md
@@ -0,0 +1,22 @@
+## index.scss creator
+
+If you create a new `index.scss` or `index.sass` it'll automatically import every SASS file in the directory, and any index.sass files from any sub-directories.
+
+For example if you have the file tree:
+
+```
+root:
+    file1.scss
+    file2.scss
+    sub-component:
+        index.scss
+        file3.scss
+```
+
+And you create an `index.scss` file in the root directory then this plugin will pre-populate the file with:
+
+```
+@import "file1";
+@import "file2";
+@import "sub-component";
+```
---
+++ src/extension.ts
@@ -0,0 +1,101 @@
+import { existsSync, lstatSync, readdirSync } from 'fs';
+import { basename, dirname, extname, join } from 'path';
+import { Disposable, ExtensionContext, window, workspace } from 'vscode';
+
+// this method is called when your extension is activated. activation is
+// controlled by the activation events defined in package.json
+export function activate(ctx: ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('index.scss creator is now active');
+
+    const templater = new Templater();
+    const controller = new TemplaterController(templater);
+
+    ctx.subscriptions.push(controller);
+    ctx.subscriptions.push(templater);
+}
+
+export class Templater {
+    public fillOutTemplateIfEmpty() {
+        const editor = window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        const doc = editor.document;
+
+        if (doc.getText().length !== 0) {
+            return;
+        }
+
+        const isSass = doc.languageId === "scss";
+
+        if (!isSass) {
+            return;
+        }
+
+        const fileName = basename(doc.fileName, extname(doc.fileName));
+
+        if (fileName !== "index") {
+            return;
+        }
+
+        const currentDirectory = dirname(doc.fileName);
+
+        const directoriesAndFiles = readdirSync(currentDirectory);
+        const fileContents: string[] = [];
+
+        directoriesAndFiles.forEach(directoryOrFile => {
+            if (directoryOrFile === basename(doc.fileName)) {
+                return; // Skip ourself
+            }
+            const path = join(currentDirectory, directoryOrFile);
+            if (lstatSync(path).isDirectory()) {
+                if (this.containsSassIndexFileSync(path)) {
+                    fileContents.push(`@import "${directoryOrFile}";`);
+                }
+            } else if (directoryOrFile.endsWith(".scss") || directoryOrFile.endsWith(".sass")) {
+                const sassFileName = basename(path, extname(path));
+                fileContents.push(`@import "${sassFileName}";`);
+            }
+        });
+
+        editor.edit(builder => {
+            builder.insert(doc.positionAt(0), fileContents.join("\n"));
+        });
+    }
+
+    private containsSassIndexFileSync(directory: string) {
+        return existsSync(join(directory, "index.scss")) || existsSync(join(directory, "index.sass"));
+    }
+
+    public dispose() {
+    }
+}
+
+class TemplaterController {
+
+    private _templater: Templater;
+    private _disposable: Disposable;
+
+    constructor(templater: Templater) {
+        this._templater = templater;
+
+        // subscribe to selection change and editor activation events
+        let subscriptions: Disposable[] = [];
+        workspace.onDidOpenTextDocument(this._onEvent, this, subscriptions);
+
+        // create a combined disposable from both event subscriptions
+        this._disposable = Disposable.from(...subscriptions);
+    }
+
+    private _onEvent() {
+        this._templater.fillOutTemplateIfEmpty();
+    }
+
+    public dispose() {
+        this._disposable.dispose();
+    }
+}
